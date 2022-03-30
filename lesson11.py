6871015c5194c179b8b45ff38bfcea84c77a92da
LordEnigMa <pacanchiked@gmail.com>
2022-03-30 21:03:57 +0400

    Homework

--- lesson11.py
+++ lesson11.py
@@ -1,9 +1,23 @@
+# def calc(num1, num2, action):
+#       if action == '+':
+#             print('Результат:', num1 + num2)
+#       elif action == '-':
+#             print('Результат:', num1 - num2)
+#       elif action == '/':
+#             print('Результат:', num1 // num2)
+#       elif action == '*':
+#             print('Результат:', num1 * num2)
+
+# calc(15, 12, '+')
+
+
+
 # def max(number1, number2):
 #       if number1 > number2:
-#             print('Наибольшое число: ' + str(number1))
+#             print('Наибольшое число:', number1)
 #       elif number1 == number2:
 #             print('Числа равны.')
 #       else:
-#             print('Наибольшое число: ' + str(number2))
+#             print('Наибольшое число:', number2)
       
 # max(15,22)
