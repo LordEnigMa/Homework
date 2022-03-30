f345a37c8f9263e0d54ab77c34196db691cf8148
LordEnigMa <pacanchiked@gmail.com>
2022-03-30 20:57:43 +0400

    Homework

---
+++ lesson11.py
@@ -0,0 +1,10 @@
+# def max(number1, number2):
+#       if number1 > number2:
+#             print('Наибольшое число: ' + str(number1))
+#       elif number1 == number2:
+#             print('Числа равны.')
+#       else:
+#             print('Наибольшое число: ' + str(number2))
+      
+
+# max(15,22)
