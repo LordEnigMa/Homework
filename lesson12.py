bbe99fb75a68a23dbda0f78bf968efc8b660b8bc
LordEnigMa <pacanchiked@gmail.com>
2022-04-02 21:16:17 +0400

    homework

--- lesson12.py
+++ lesson12.py
@@ -29,41 +29,45 @@
 
 #####################################################
 
-def calc():
-      action = ''
-      nums = []
+# def calc():
+#       action = ''
+#       nums = []
       
-      try:
-            string = input('Введите действие: ')
-            if ' ' in string:
-                  print('Напишите без пробелов!')
-            if len(string) <= 2:
-                  calc()
-            for i in range(1, len(string)):
-                  if string[i] == '.':
-                        continue
-                  if string[i].isdigit():
-                        continue
-                  else:
-                        nums.append(float(string[:i]))
-                        action = string[i]
-                        nums.append(float(string[i+1:]))
-                        break
-            if len(nums) < 2:
-                  calc()
-      except:
-            calc()
+#       try:
+#             string = input('Введите действие: ')
+#             if ' ' in string:
+#                   print('Напишите без пробелов.')
+#                   calc()
+#             if ',' in string:
+#                   print('Нецелые числа писать через точку.')
+#                   calc()
+#             if len(string) <= 2:
+#                   calc()
+#             for i in range(1, len(string)):
+#                   if string[i] == '.':
+#                         continue
+#                   if string[i].isdigit():
+#                         continue
+#                   else:
+#                         nums.append(float(string[:i]))
+#                         action = string[i]
+#                         nums.append(float(string[i+1:]))
+#                         break
+#             if len(nums) < 2:
+#                   calc()
+#       except:
+#             calc()
       
-      if action == '+':
-            print('Результат: ', nums[0] + nums[1])
-      if action == '-':
-            print('Результат: ', nums[0] - nums[1])
-      if action == '*':
-            print('Результат: ', nums[0] * nums[1])
-      if action == '/':
-            if nums[0] != 0:
-                  calc()
-            else:
-                  print('Результат: ', nums[0] / nums[1])
+#       if action == '+':
+#             print('Результат: ', nums[0] + nums[1])
+#       if action == '-':
+#             print('Результат: ', nums[0] - nums[1])
+#       if action == '*':
+#             print('Результат: ', nums[0] * nums[1])
+#       if action == '/':
+#             if nums[0] != 0:
+#                   calc()
+#             else:
+#                   print('Результат: ', nums[0] / nums[1])
                   
-calc()
+# calc()
