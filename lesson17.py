3d96699a44972397c7c8df8c05ca07dd7dc01be4
LordEnigMa <pacanchiked@gmail.com>
2022-04-24 13:11:49 +0400

    homework

--- lesson17.py
+++ lesson17.py
@@ -91,3 +91,48 @@
 
 ############################################
 
+# def Lucky(n):
+      
+#       if len(str(n)) % 2 == 0:
+#             length = len(str(n)) // 2
+#             n = str(n)
+#             part1 = n[:length]
+#             part2 = n[length:]
+#             sum1 = 0
+#             sum2 = 0
+
+#             for i in part1:
+#                   sum1 += int(i)
+#             for i in part2:
+#                   sum2 += int(i)
+            
+#             if sum1 == sum2:
+#                   print('Счастливый билет!')
+#             else:
+#                   print('Не счастливый билет!')
+
+#       else:
+#             print('Не счастливый билет!')
+
+# Lucky(1230)
+
+############################################
+
+def sort_by_height(mylist):
+      new_list = []
+      indexes = []
+
+      for i in range(len(mylist)):
+            if int(mylist[i]) != -1:
+                  new_list.append(int(mylist[i]))
+            else:
+                  indexes.append(int(i))
+
+      new_list.sort()
+
+      for i in indexes:
+            new_list.insert(i, -1)
+
+      print(new_list)
+
+sort_by_height([-1, 150, 190, 170, -1, -1, 160, 180])
